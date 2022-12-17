2fe516a1ddba9268c194a02dc0a81c13d743f155
cwp-alerts1 <103568629+cwp-alerts1@users.noreply.github.com>
2022-12-17 14:53:28 +0530

    Update sq_ES9200test.tf

--- sq_ES9200test.tf
+++ sq_ES9200test.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "example" {
+resource "aws_security_group" "example1" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.main.id
