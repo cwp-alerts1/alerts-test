a3cf840fa3775416b2bcd029fc23da72694491bf
cwp-alerts1 <103568629+cwp-alerts1@users.noreply.github.com>
2023-02-09 11:29:08 +0530

    Create EC2_secrets.tf

---
+++ Scan_path/EC2_secrets.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web" {
+    ami           = "ami-830c94e3"
+    instance_type = "t2.micro"
+    user_data = "access_key=123456ABCDEFGHIJZTLA and secret_key=AAAaa+Aa4AAaAA6aAkA0Ad+Aa8aA1aaaAAAaAaA"
+    ingress_cidr_blocks = ["0.0.0.0/16"]
+    tags = {
+    Name = "ExampleAppServerInstance"
+   } 
+   
+}
